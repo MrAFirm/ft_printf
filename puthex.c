4e256a666129e3f689f67c140e85d254840ecb95
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2024-07-15 23:17:14 +0800

    Formatted puthex and fixed some minor errors.
    Fixefd some variable names from lower to upper, and formatted some of the code for readability.

--- puthex.c
+++ puthex.c
@@ -6,7 +6,7 @@
 /*   By: lkhye-ya <lkhye-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 17:44:41 by lkhye-ya          #+#    #+#             */
-/*   Updated: 2024/07/15 21:26:25 by lkhye-ya         ###   ########.fr       */
+/*   Updated: 2024/07/15 23:15:38 by lkhye-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,19 @@
 int	ft_puthex_upper(unsigned int num, char hex)
 {
 	char			*hex_chars_upper;
-	char			*hex_chars_lower;
 	unsigned long	count;
 	unsigned char	format;
 
 	format = hex;
 	count = 0;
-	hex_chars_lower = "0123456789ABCDEF";
+	hex_chars_upper = "0123456789ABCDEF";
 	if (format == 'X')
 	{
 		if (num >= 16)
 		{
 			count += ft_puthex_upper((num / 16), hex);
 			ft_putchar(hex_chars_upper[num % 16]);
-		count++;
+			count++;
 		}
 		else
 		{
@@ -41,7 +40,6 @@ int	ft_puthex_upper(unsigned int num, char hex)
 
 int	ft_puthex_lower(unsigned int num, char hex)
 {
-	char			*hex_chars_upper;
 	char			*hex_chars_lower;
 	unsigned long	count;
 	unsigned char	format;
@@ -55,7 +53,7 @@ int	ft_puthex_lower(unsigned int num, char hex)
 		{
 			count += ft_puthex_lower((num / 16), hex);
 			ft_putchar(hex_chars_lower[num % 16]);
-		count++;
+			count++;
 		}
 		else
 		{
@@ -70,7 +68,7 @@ int	ft_puthex_lower(unsigned int num, char hex)
 #include <stdio.h>
 int main()
 {
-	int hi = 17;
+	int hi = 12;
 	printf("hexdecimal: %d\n", ft_puthex_lower(hi, 'x'));
 }
 */
