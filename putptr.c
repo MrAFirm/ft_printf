022a91b7f7cdd9a91aa6f31989283d6bbcc7dccf
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2024-07-16 19:10:24 +0800

    Added putptr.c
    Not sure if it's working but it's worth a shot as I coded this from home.

--- putptr.c
+++ putptr.c
@@ -1,14 +1,33 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   putptr.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lkhye-ya <lkhye-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/12 17:43:50 by lkhye-ya          #+#    #+#             */
-/*   Updated: 2024/07/12 17:44:00 by lkhye-ya         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "ft_printf"
 
-#include "ft_printf.h"
+int ft_putptr(void *ptr)
+{
+    int *int_ptr;
+    int value;
+    int dereferencing;
 
+    int_ptr = (int *)ptr;
+    *ptr = &value;
+    dereferencing = *ptr;
+    return (dereferencing);
+}
+
+int ft_putchar(int c)
+{
+    char i;
+    i = (char)c;
+    write(1, &i, 1);
+    return (1);
+}
+
+int ft_puthex(unsigned int num)
+{
+    char *hex_chars;
+    hex_chars = "0123456789abcdef";
+    count = 0;
+    if (num >= 16)
+        count += ft_puthex(num / 16);
+    else ft_putchar(num % 16);
+    count++;
+    return (count);
+}
