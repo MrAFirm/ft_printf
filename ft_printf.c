f83612cfb410622d4883475d67768adc0afede6e
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2024-07-18 18:47:18 +0800

    Finished ft_printf.c
    Haven't tested but finished ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,10 +6,52 @@
 /*   By: lkhye-ya <lkhye-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 16:58:09 by lkhye-ya          #+#    #+#             */
-/*   Updated: 2024/07/12 16:58:11 by lkhye-ya         ###   ########.fr       */
+/*   Updated: 2024/07/18 18:45:07 by lkhye-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...);
+static int	print_format(char c, va_list arg)
+{
+	char	*format;
+	
+	format = c;
+	if (*format == 'c')
+		return (ft_putchar(va_arg(arg, int)));
+	else if (*format == 's')
+		return (ft_putstr(va_arg(arg, char *)));
+	else if (*format == 'p')
+		return (ft_putptr(va_arg(arg, void *)));
+	else if (*format == 'd' || format == 'i')
+		return (ft_putnbr(va_arg(arg, int)));
+	else if (*format == 'u')
+		return (ft_putuint(va_arg(arg, unsigned int)));
+	else if (*format == 'x')
+		return (ft_puthex_lower(va_arg(arg, unsigned int), 'x'));
+	else if (*format == 'X')
+		return (ft_puthex_upper(va_arg(arg, unsigned int), 'X'));
+	else
+		return (write(1, &c, 1));
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		count;
+
+	count = 0;
+	if (format == NULL)
+		return (1);
+	va_start(args, format);
+	count = printformat(format, args);
+	format++;
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1))
+			count += printformat(format, args);
+		count++;
+	}
+	va_end(args);
+	return (count);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lkhye-ya <lkhye-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 16:58:05 by lkhye-ya          #+#    #+#             */
-/*   Updated: 2024/07/17 15:52:27 by lkhye-ya         ###   ########.fr       */
+/*   Updated: 2024/07/18 18:43:22 by lkhye-ya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 
 # include <stdarg.h>
 # include <unistd.h>
-# include <stddef.h>
 # include <stdint.h>
 # include <stdlib.h>
 
+size_t			ft_strlen(const char *str);
 int				ft_putnbr(int n);
 int				ft_putchar(int c);
 int				ft_putstr(char *s);
 int				ft_putuint(unsigned int	n);
 int				ft_puthex_upper(unsigned int num, char hex);
 int				ft_puthex_lower(unsigned int num, char hex);
-
-
-
+int 			ft_putptr(void *ptr);
+int				ft_printf(const char *format, ...);
 #endif
